2a9733e15620ae82021fffaed7b7e31435dcbb35
Karl Montenius <karl.montenius@hotmail.com>
2025-01-14 06:42:35 +0100

    Klaviyo integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/globals.css"; // Global styles
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script"; // Importera Next.js Script
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Analytics />
+      <Script
+        strategy="afterInteractive"
+        src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=TBEeQc"
+      />
+
       <Component {...pageProps} />
     </QueryClientProvider>
   );
