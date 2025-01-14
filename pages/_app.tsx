78639824c03ac8019f9aca00280dc79e120d36be
Karl Montenius <karl.montenius@hotmail.com>
2025-01-14 10:24:19 +0100

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "../styles/globals.css"; // Global styles
+import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script"; // Importera Next.js Script
+import Script from "next/script";
 import { Footer } from "components/Footer";
 import NewsletterSignup from "components/NewsletterSignup";
 import { Navigation } from "components/Navigation";
@@ -12,11 +12,34 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      {/* Vercel Analytics */}
+
       <Analytics />
+
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-ENYBNDCDBG`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ENYBNDCDBG');
+          `,
+        }}
+      />
+
+      {/* Klaviyo Script */}
       <Script
         strategy="afterInteractive"
         src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=TBEeQc"
       />
+
       <Navigation />
       <Component {...pageProps} />
       <NewsletterSignup />
