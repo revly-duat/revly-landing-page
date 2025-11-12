c093cb5fd363b7465e6db68868febadefa178efc
Karl Montenius <karl.montenius@hotmail.com>
2025-11-12 11:36:24 +0100

    Apollo tracker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,6 +27,26 @@ function MyApp({ Component, pageProps }: AppProps) {
         type="text/javascript"
         async
       ></Script>
+      {/* Apollo Tracker */}
+      <Script
+        id="apollo-tracker"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      (function initApollo() {
+        var n = Math.random().toString(36).substring(7);
+        var o = document.createElement("script");
+        o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+        o.async = true;
+        o.defer = true;
+        o.onload = function() {
+          window.trackingFunctions.onLoad({ appId: "691443c4060e0a000d494653" });
+        };
+        document.head.appendChild(o);
+      })();
+    `,
+        }}
+      />
       {/* Google Analytics */}
       <Script
         strategy="afterInteractive"
